bfd1964019a417d7e258ad33e6d795fb2748e8d6
MedinaSilva <k.medinasilva1234@gmail.com>
2023-03-21 18:17:19 -0600

    Primera sesion de codificacion en vivo

---
+++ app.js
@@ -0,0 +1,8 @@
+const arreglo =["hola","adios","java","javascript","anticonstitucional","si","pasado","programar","comer","jugar"];
+function palabras (string, arr) {
+    let i =0;
+    if (arr[i].length> string){
+        console.log()
+    }
+}
+palabras ("gato",arreglo);
